271048b59d9841d8ba13195fd256623dcde7f695
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-10-22 16:19:23 -0700

    get now takes a temp dir, and uses it to write its files. python -m afloat.ofx looks in there

--- afloat/ofx/__init__.py
+++ afloat/ofx/__init__.py
@@ -2,8 +2,16 @@
 Negotiating the network protocol and file format of OFX
 """
 
+
 if __name__ == '__main__':
     from afloat.ofx import get, parse
-    o = get.optionsRun(['get', 'EECU', '11340704', '11340704=8', '1'])
-    o = {'outFilename': 'eecu20071020.ofx'}
-    parse.run(['parse', o['outFilename']])
+    import tempfile
+    import shutil
+    tempdir = tempfile.mkdtemp()
+    try:
+        get.run(['get', '--outdir', tempdir, 'EECU', '11340704', 
+            '11340704=8', '1', ])
+        parse.run(['parse', tempdir + '/account.ofx'])
+        parse.run(['parse', tempdir + '/statement.ofx'])
+    finally:
+        pass # shutil.rmtree(tempdir)
--- afloat/ofx/get.py
+++ afloat/ofx/get.py
@@ -30,7 +30,9 @@ get --listSites
 ___
 """
     optFlags = [['listSites', 'l', 'List all available bank sites']]
-    optParameters = [['out', 'o', 'Output filename']]
+    optParameters = [['outdir', 'd', '.', 
+        'Directory to put output files (statement.ofx, account.ofx)'],
+        ]
 
     def parseArgs(self, siteName=None, user=None, account=None, accountType=None):
         if self['listSites']:
@@ -47,7 +49,7 @@ ___
         self['accountType'] = accountType
 
     def opt_listSites(self):
-        joined = join("\n", ['\t'.join(i) for i in shortSites.items()])
+        raise NotImplemented
         print "AVAILABLE SITES:\n", joined
         self['listSites'] = True
 
@@ -77,14 +79,14 @@ ___
 
         passwd = getpass(prompt="Password (%s at %s): " % (user, site.org))
 
-        command1 = accountInfoCommand(self['user'], passwd, site)
+        command1 = accountInfoCommand(self['user'], passwd, site, self['outdir'])
         d1 = getProcessOutputUtil(command1)
 
-        command2 = statementCommand(self['user'], passwd, site, account, acType)
+        command2 = statementCommand(self['user'], passwd, site, self['outdir'],
+                account, acType)
         d1.addCallback(lambda _: getProcessOutputUtil(command2))
 
         return d1
-
 
 
 class EECU(object):
@@ -94,24 +96,24 @@ class EECU(object):
     org = "Educational Employees C U"
     
 
-def accountInfoCommand(user, password, bank):
+def accountInfoCommand(user, password, bank, outdir):
     c = '''ofxconnect -a --user=%(user)s --pass="%(pass)s" --fid=%(fid)s 
         --bank=%(bank)s --org="%(org)s" --url=%(url)s
-        account.ofx'''
+        "%(outdir)s"/account.ofx'''
     d = bank.__dict__.copy()
-    d.update( {'user': user, 'pass': password,} )
+    d.update( {'user': user, 'pass': password, 'outdir': outdir} )
     c = c % d
     return c
 
 
-def statementCommand(user, password, bank, account, accountType):
+def statementCommand(user, password, bank, outdir, account, accountType):
     c = '''ofxconnect -s --user=%(user)s --pass="%(pass)s" --fid=%(fid)s 
         --bank=%(bank)s --org="%(org)s" --acct=%(account)s 
         --type=%(accountType)s --past=30 --url=%(url)s
-        statement.ofx'''
+        %(outdir)s/statement.ofx'''
     d = bank.__dict__.copy()
     d.update( {'user': user, 'pass': password, 'account': account,
-        'accountType': accountType})
+        'accountType': accountType, 'outdir': outdir})
     c = c % d
     return c
 
@@ -129,21 +131,6 @@ def run(argv=None):
 
     return 0
 
-def optionsRun(argv=None):
-    """
-    Run the command, and return the options instead
-    """
-    if argv is None:
-        argv = sys.argv
-    o = Options()
-    try:
-        o.parseOptions(argv[1:])
-    except usage.UsageError, e:
-        print str(o)
-        print str(e)
-        return o
-
-    return o
 
 if __name__ == '__main__': sys.exit(run())
 
