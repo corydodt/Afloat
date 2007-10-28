e49ac3ed8da06d12a7d4db610dc1bff192d2fee4
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-10-27 19:07:22 -0700

    option to use an already-downloaded statement instead of downloading

--- afloat/ofx/__init__.py
+++ afloat/ofx/__init__.py
@@ -4,14 +4,21 @@ Negotiating the network protocol and file format of OFX
 
 
 if __name__ == '__main__':
+    import sys
     from afloat.ofx import get, parse
     import tempfile
     import shutil
-    tempdir = tempfile.mkdtemp()
+    if len(sys.argv) > 1:
+        tempdir = sys.argv[1]
+        reloading = 1
+    else:
+        tempdir = tempfile.mkdtemp()
+        reloading = 0
+
     try:
-        get.run(['get', '--outdir', tempdir, 'EECU', '11340704', 
-            '11340704=8', '1', ])
-        parse.run(['parse', tempdir + '/account.ofx'])
-        parse.run(['parse', tempdir + '/statement.ofx'])
+        if not reloading:
+            get.run(['get', '--outdir', tempdir, 'EECU', '11340704', 
+                '11340704=8', '1', ])
+        parse.run(['parse', tempdir])
     finally:
-        pass # shutil.rmtree(tempdir)
+        shutil.rmtree(tempdir)
