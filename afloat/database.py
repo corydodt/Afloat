d80703f60398cfa312f0afe6fba440fea2a019c9
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-10-28 21:56:03 -0700

    module manages the retrieval of data into the storm database

--- afloat/database.py
+++ afloat/database.py
@@ -1,13 +1,21 @@
 """
-Interact with the local storm database
+Interact with the local storm database.  Retrieve OFX, google events, etc. into
+storm tables
 """
+import glob
+import tempfile
+import shutil
+
+from twisted.internet import defer
+from twisted.python import log
+
 from storm import locals
 
 from afloat.util import RESOURCE
 
 class BankTransaction(object):
     __storm_table__ = 'banktxn'
-    id = locals.RawStr(primary=True)
+    id = locals.Unicode(primary=True)
     account = locals.Unicode()
     type = locals.Unicode()
     amount = locals.Int() # stored in cents
@@ -17,6 +25,7 @@ class BankTransaction(object):
     checkNumber = locals.Int()
     # ledgerBalance ? store the ledger balance after this txn, to verify?
 
+
 class Hold(object):
     __storm_table__ = 'hold'
     id = locals.Int(primary=True)
@@ -24,6 +33,7 @@ class Hold(object):
     amount = locals.Int() # cents!
     description = locals.Unicode()
     dateApplied = locals.Date()
+
 
 class ScheduledTransaction(object):
     __storm_table__ = 'scheduledtxn'
@@ -45,6 +55,7 @@ class NetworkLog(object):
     description = locals.Unicode()
     severity = locals.Unicode() # OK or ERROR
 
+
 def createTables():
     import os
     os.system('sqlite3 -echo afloat.db < %s' % (RESOURCE('tables.sql'),))
@@ -53,6 +64,75 @@ def initializeStore():
     db = locals.create_database('sqlite:///%s' % (RESOURCE('afloat.db'),))
     store = locals.Store(db)
     return store
+
+def getOfx(store, *requests, **kw):
+    """
+    Retrieve, then parse, the OFX into the storm database
+
+    requests is a list of callables.  When called with a directory name, they
+    should return a deferred which fires when the request is finished.
+
+    The callable should write a file named *.ofx to the directory it was
+    passed.
+
+    kwargs:
+        ofxEncoding: the encoding used in the ofx files to be retrieved (may
+                     be set in config.py originally)
+    """
+    ofxEncoding = kw['ofxEncoding']
+
+    def withTempdir(outdir):
+        d = defer.Deferred()
+        for request in requests:
+            cb = lambda _, req=request: req(outdir)
+            d.addCallback(cb)
+
+        def gotOfxFiles(_):
+            from afloat.ofx.parse import OFXParser
+            p = OFXParser()
+            p.ofxEncoding = ofxEncoding
+            # TODO - get passed-in self['debug'] ?
+            ## p.debug = self['debug']
+
+            for ofx in glob.glob(outdir + '/account*.ofx'):
+                doc = open(ofx).read()
+                p.feed(doc)
+            for ofx in glob.glob(outdir + '/statement*.ofx'):
+                doc = open(ofx).read()
+                p.feed(doc)
+
+            for account in p.banking.accounts.values():
+                for txn in account.transactions.values():
+                    newTransaction(store, account.id, txn)
+            store.commit()
+            # TODO - matchups
+            # TODO - create storm objects for Holds
+            return p.banking
+
+        d.addCallback(gotOfxFiles).addErrback(log.err)
+        d.callback(None)
+        return d
+
+    tempdir = tempfile.mkdtemp()
+    return withTempdir(tempdir) # .addBoth(lambda _: shutil.rmtree(tempdir))
+
+
+def newTransaction(store, accountId, txn):
+    if not store.get(BankTransaction, txn.id):
+        bankTxn = BankTransaction()
+        bankTxn.id = txn.id
+        bankTxn.account = accountId
+        bankTxn.type = txn.type
+        bankTxn.amount = txn.amount
+        # TODO userDate = 
+        bankTxn.ledgerDate = txn.date
+        bankTxn.memo = txn.memo
+        # TODO checkNumber = 
+        # TODO ledgerBalance? = 
+        store.add(bankTxn)
+
+def getGvents(store):
+    pass # TODO
 
 if __name__ == '__main__':
     createTables()
