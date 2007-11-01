ecb8c10f21b87b5e2f8eaea94f57c70e765a30e2
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-10-31 21:37:45 -0700

    don't bother with an intermediate file

--- doc/curl.sh
+++ doc/curl.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -e
-python formatofxreq.py input.ofx.in config.py > input.ofx
-curl --data-binary @input.ofx -H "Connection: close" -H "Content-Type: application/x-ofx" https://www.eecuonline.org/scripts/isaofx.dll
+python formatofxreq.py input.ofx.in config.py | curl --data-binary @- -H "Connection: close" -H "Content-Type: application/x-ofx" https://www.eecuonline.org/scripts/isaofx.dll
